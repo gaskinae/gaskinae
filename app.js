b11ddaa727d2825ddd601132ddd56918fa80c397
Amy Gaskin <GaskinAE@cardiff.ac.uk>
2024-01-21 14:55:18 +0000

    Remove JS animated scrolling for now, remove website subpage, add README.md

--- app.js
+++ app.js
@@ -22,38 +22,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-$(document).ready(function () {
-    // Enable navigation with a clickable menu icon
-    $("#menu").on("click", function () {
-        $("#menu").css("opacity", "0");
-        $("#lgMenu").addClass("enter");
-    });
-
-    $("#exit").on("click", function () {
-        $("#lgMenu").removeClass("enter");
-        $("#menu").css("opacity", "1");
-    });
-
-    // Smooth scrolling for anchor links
-    $('a.nav-link').on('click', function (event) {
-        var target = $($(this).attr('href'));
-
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const skillIcons = document.querySelectorAll('.skill-icon');
-
-    skillIcons.forEach(icon => {
-        const tooltip = document.createElement('div');
-        tooltip.classList.add('tooltip');
-        tooltip.textContent = icon.getAttribute('data-tooltip');
-        icon.appendChild(tooltip);
-    });
-});
